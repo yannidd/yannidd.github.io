d98f1da4396b874283fd0230da96848a25e39b3c
yannidd <yanislavdonchev@gmail.com>
2019-09-15 16:49:11 +0100

    Created auto numbering/cross-referencing library.

---
+++ assets/js/posts/numberize.js
@@ -0,0 +1,71 @@
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
+
+// Go through all h2 tags.
+$('h2').each(function(h2_index) {
+  var label = (h2_index + 1) + '. ';
+  var text = $(this).html();
+  // Add index to h2 elements.
+  $(this).prepend(label);
+  $(this).attr('label', label.trim());
+  $(this).attr('text', text);
+  
+  // Go through all h3 tags between two h2 tags.
+  $(this).nextUntil('h2', 'h3').each(function(h3_index) {
+    var label = (h2_index + 1) + '.' + (h3_index + 1) + '. ';
+    var text = $(this).html();
+    // Add index to h3 elements.
+    $(this).prepend(label);
+    $(this).attr('label', label.trim());
+    $(this).attr('text', text);
+    
+    // Go through all h4 tags between two h3 tags.
+    $(this).nextUntil('h3', 'h4').each(function(h4_index) {
+      var label = (h2_index + 1) + '.' + (h3_index + 1) + '.' + (h4_index + 1) + '. ';
+      var text = $(this).html();
+      // Add index to h4 elements.
+      $(this).prepend(label);
+      $(this).attr('label', label.trim());
+      $(this).attr('text', text);
+    });
+  });
+  
+  // Go through all figure tags between two h2 tags.
+  $(this).nextUntil('h2', 'figure').each(function(figure_index) {
+    var label = (h2_index + 1) + '.' + (figure_index + 1) + '.  ';
+    var text = $(this).html();
+    // Add index to figcaption elements.
+    $(this).children('figcaption').prepend('Figure ' + label);
+    $(this).attr('label', label.trim());
+    $(this).attr('text', text);
+  });
+});
+
+// Find all leaf nodes inside the article tag.
+$('article').find('*:not(:has(*))').each(function() {
+  var text = $(this).text();
+
+  // Check if the node has '\label_of{...}'.
+  var matches = text.match(/\\label_of{.+}/);
+  if (matches) {
+    // Go through each match.
+    $.each(matches, function(index, value) {
+      // Find the reference and its label.
+      var ref_id = value.replace('\\label_of{', '').replace('}', '');
+      var label = $(ref_id).attr('label');
+
+      // Replace with label if reference exists, replace with error otherwise.
+      if (label) {
+        text = text.replace(value, label);
+      }
+      else {
+        text = text.replace(value, 
+          '<span style="color: red;"> \
+            ERROR: Undefined reference ' + ref_id +
+          '</span>');
+      }
+    });
+
+    // Update with the new text;
+    $(this).html(text);
+  }
+});
